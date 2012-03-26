b3c7f02714e8e6fd5a591cc3a33dd9510e05ce9d
James Rodgers <james.rodgers@guardian.co.uk>
2012-03-26 10:33:23 +0100

    Removed dependency on scalaxb

--- src/main/java/harness/Harness.java
+++ src/main/java/harness/Harness.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-
+// TODO: move this to tests
 public class Harness
 {
     private static final String accountName = "gnmtestuser";
